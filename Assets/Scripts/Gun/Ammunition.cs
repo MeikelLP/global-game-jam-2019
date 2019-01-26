702b320e077387e0372f779182ee45b8471eb3da
Kevin Lange <kevin.lange.1991@gmai.com>
2019-01-26 12:39:42 +0100

    Added Gun Classes

---
+++ Assets/Scripts/Gun/Ammunition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammunition : MonoBehaviour
+{
+
+    public int min;
+    public int max;
+    public int current;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Decrease()
+    {
+        if (current >= 0)
+        {
+            current--;    
+        }
+    }
+
+    public bool isEmpty()
+    {
+        return current == 0;
+    }
+
+    void Reload()
+    {
+        current = max;
+    }
+}
