0dbd2a255a0b9a502e64bb417e0d8f79fab55bd1
Kevin Lange <kevin.lange.1991@gmai.com>
2019-01-26 12:17:34 +0100

    Logging
    Added Logging in Player Shooting
    Added testobjects to hit in scene

--- Assets/_Complete-Game/Scripts/Player/PlayerShooting.cs
+++ Assets/_Complete-Game/Scripts/Player/PlayerShooting.cs
@@ -93,11 +93,13 @@ namespace CompleteProject
             // Enable the line renderer and set it's first position to be the end of the gun.
             gunLine.enabled = true;
             gunLine.SetPosition (0, transform.position);
+            
 
             // Set the shootRay so that it starts at the end of the gun and points forward from the barrel.
             shootRay.origin = transform.position;
             shootRay.direction = transform.forward;
 
+            Debug.Log("check if something was hit");
             // Perform the raycast against gameobjects on the shootable layer and if it hits something...
             if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
             {
@@ -107,16 +109,22 @@ namespace CompleteProject
                 // If the EnemyHealth component exist...
                 if(enemyHealth != null)
                 {
+                    Debug.Log("enemy was hit");       
                     // ... the enemy should take damage.
                     enemyHealth.TakeDamage (damagePerShot, shootHit.point);
                 }
-
+                else
+                {
+                    Debug.Log("no enemy was hit");
+                }
+                
                 // Set the second position of the line renderer to the point the raycast hit.
                 gunLine.SetPosition (1, shootHit.point);
             }
             // If the raycast didn't hit anything on the shootable layer...
             else
             {
+                Debug.Log("nothing with the shootable layer has been hit.");
                 // ... set the second position of the line renderer to the fullest extent of the gun's range.
                 gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
             }
