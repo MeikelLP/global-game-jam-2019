2de3a6ef11e8febabca048ef63fc668cba0bf5fe
Kevin Lange <test@gmail.com>
2019-01-27 12:02:17 +0100

    added script for stealing behaviour

---
+++ Assets/Scripts/Enemy/StealingBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Enemy;
+using UnityEngine;
+
+public class StealingBehaviour : MonoBehaviour
+{
+    Animator anim;
+
+    EnemyHealth enemyHealth;
+    float timer;
+    
+    Behaviour closestStealable;
+    private bool stealableInRange;
+
+    void Awake()
+    {
+        enemyHealth = GetComponent<EnemyHealth>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    void FixedUpdate()
+    {
+        closestStealable = EnemyTargetFinder.FindClosestStealable(enemyHealth.transform.position);
+    }
+    
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == closestStealable.gameObject)
+        {
+            stealableInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == closestStealable.gameObject)
+        {
+            stealableInRange = false;
+        }
+    }
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        // TODO steal
+    }
+}
